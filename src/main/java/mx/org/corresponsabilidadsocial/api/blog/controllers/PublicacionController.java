3096479f5fac1e6f2ebfa6fd3c4d1846bbb0beb6
ametrey <aetcheverryrey@gmail.com>
2022-03-24 12:47:19 -0300

    Crear test obtener lista publicaciones
    Se crea test del metodo en service para traer
    la lista de publicaciones desde el repo

--- src/main/java/mx/org/corresponsabilidadsocial/api/blog/controllers/PublicacionController.java
+++ src/main/java/mx/org/corresponsabilidadsocial/api/blog/controllers/PublicacionController.java
@@ -4,20 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import mx.org.corresponsabilidadsocial.api.blog.services.PublicacionService;
 
 @RestController
+@RequestMapping("/blog")
 public class PublicacionController {
 
     @Autowired
     PublicacionService publicacionService;
 
     @GetMapping
-    public ResponseEntity<?> Post() throws Exception{
+    public ResponseEntity<?> traerListaPublicaciones() throws Exception{
 
         return new ResponseEntity<>(publicacionService.traerListaPublicaciones(), HttpStatus.OK);
     
---
+++ src/test/java/mx/org/corresponsabilidadsocial/api/blog/controllers/ServiceTest.java
@@ -0,0 +1,43 @@
+package mx.org.corresponsabilidadsocial.api.blog.controllers;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import mx.org.corresponsabilidadsocial.api.blog.entities.Publicacion;
+import mx.org.corresponsabilidadsocial.api.blog.repositories.PublicacionRepository;
+import mx.org.corresponsabilidadsocial.api.blog.services.PublicacionService;
+
+@SpringBootTest
+public class ServiceTest {
+
+    @Autowired
+    private PublicacionService publicacionService;
+
+    @DisplayName("JUnit test para chequear que se devuelva lsita con publicaciones")
+    @Test
+    public void givenListaPublicaciones_whenTraerListaPublicaciones_thenListaPublicaciones(){
+        
+       
+
+        Publicacion publicacion1 = new Publicacion(1, "titulo", "imagen", "cuerpoTexto", "fechaPublicacion", true);
+        Publicacion publicacion2 = new Publicacion(1, "tdsadsa", "idsadsan", "cASDSAto", "fTREWTRacion", true);
+
+        PublicacionRepository.publicaciones.add(publicacion1);
+        PublicacionRepository.publicaciones.add(publicacion2);
+               
+              
+        List<Publicacion> ListaPublicacion = publicacionService.traerListaPublicaciones();
+
+       
+        assertThat(ListaPublicacion).isNotNull();
+        assertThat(ListaPublicacion.size()).isEqualTo(2);
+
+    }
+    
+}
