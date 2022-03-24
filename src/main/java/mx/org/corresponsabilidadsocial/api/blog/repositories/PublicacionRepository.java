55c9b8377a387b5531df332ae3678f2b9de709bd
ametrey <aetcheverryrey@gmail.com>
2022-03-23 21:34:29 -0300

    Crear capa service
    Se crea la capa service con el metodo necesario para el endpoint

---
+++ src/main/java/mx/org/corresponsabilidadsocial/api/blog/repositories/PublicacionRepository.java
@@ -0,0 +1,24 @@
+package mx.org.corresponsabilidadsocial.api.blog.repositories;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import mx.org.corresponsabilidadsocial.api.blog.entities.Publicacion;
+
+@Repository
+public class PublicacionRepository {
+
+    public static List<Publicacion> publicaciones = new ArrayList<>();
+
+    
+
+    
+	
+    
+
+ 
+  
+}
