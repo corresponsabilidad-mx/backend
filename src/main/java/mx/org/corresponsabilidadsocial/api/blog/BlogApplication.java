662f46a5bae297713d07faedbd97e07daf8a7b78
ametrey <aetcheverryrey@gmail.com>
2022-03-23 21:36:05 -0300

    Cargar lista con un ejemplo
    Se carga la lista con un ejemplo para ser testeado en postman
    y posteriormente hacer unit testing

--- src/main/java/mx/org/corresponsabilidadsocial/api/blog/BlogApplication.java
+++ src/main/java/mx/org/corresponsabilidadsocial/api/blog/BlogApplication.java
@@ -1,13 +1,27 @@
 package mx.org.corresponsabilidadsocial.api.blog;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import mx.org.corresponsabilidadsocial.api.blog.entities.Publicacion;
+import mx.org.corresponsabilidadsocial.api.blog.repositories.PublicacionRepository;
+
+
+
 
 @SpringBootApplication
 public class BlogApplication {
 
 	public static void main(String[] args) {
+
+		
+		
+		PublicacionRepository.publicaciones.add(new Publicacion(1, "titulo", "imagen", "cuerpoTexto", "fechaPublicacion", true));
+		
 		SpringApplication.run(BlogApplication.class, args);
+		
 	}
 
 }
---
+++ src/test/java/mx/org/corresponsabilidadsocial/services/PublicacionServiceTest.java
@@ -0,0 +1,14 @@
+package mx.org.corresponsabilidadsocial.services;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+
+
+@ExtendWith(MockitoExtension.class)
+public class PublicacionServiceTest {
+
+}
