70e0e17ae5f8d42c58406827d894ed259652db65
ametrey <aetcheverryrey@gmail.com>
2022-04-03 15:01:29 -0300

    Arreglar mensaje resultado validacion
    Se arregla el formato de mensaje ahora muestra la clave y el mesaje correspondiente

--- src/main/java/mx/org/corresponsabilidadsocial/api/blog/exceptions/ControllerExceptionHandler.java
+++ src/main/java/mx/org/corresponsabilidadsocial/api/blog/exceptions/ControllerExceptionHandler.java
@@ -2,18 +2,12 @@ package mx.org.corresponsabilidadsocial.api.blog.exceptions;
 
 import java.util.*;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.HttpClientErrorException.BadRequest;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.validation.ObjectError;
+import org.springframework.validation.*;
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
@@ -42,11 +36,12 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         @Override
         protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                         HttpHeaders headers, HttpStatus status, WebRequest request) {
-                List<String> details = new ArrayList<>();
-                for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-                        details.add(error.getDefaultMessage());
-                }
-                ErrorMessage error = new ErrorMessage(details);
-                return new ResponseEntity<>(error.getDetails(), HttpStatus.BAD_REQUEST);
+                Map<String, String> errors = new LinkedHashMap<>();
+                ex.getBindingResult().getAllErrors().forEach((error) -> {
+                        String fieldName = ((FieldError) error).getField();
+                        String errorMessage = error.getDefaultMessage();
+                        errors.put(fieldName, errorMessage);
+                });
+                return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
         }
 }
