94c694011d0f22a62281ec3d763fba9d43e91f28
GabrielaSoruco <soruco.gabi@gmail.com>
2022-04-20 18:59:29 -0300

    crear clase firebase initializer

---
+++ src/main/java/mx/org/corresponsabilidadsocial/api/blog/services/FirebaseInitializer.java
@@ -0,0 +1,36 @@
+package mx.org.corresponsabilidadsocial.api.blog.services;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+@Service
+public class FirebaseInitializer {
+
+    @PostConstruct
+    private void initDB() throws IOException {
+
+        ClassLoader  classLoader = FirebaseInitializer.class.getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("blog-csm-firebase-adminsdk.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://blog-csm.firebaseio.com/")
+                .build();
+        FirebaseApp.initializeApp(options);
+    }
+
+    public Firestore getFirestore(){
+        return FirestoreClient.getFirestore();
+    }
+}
